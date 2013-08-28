3d114362573d71ca80fd5691f4af20c679f79108
Linda Xie <lindaxie93@gmail.com>
2013-08-28 03:00:10 -0400

    responsive contact bar; carousel height adjustment

--- public/js/linda-script.js
+++ public/js/linda-script.js
@@ -21,7 +21,7 @@ $(function(){
 
 		// Scroll to the correct position
 		var top = $(new_selected_id).offset().top;
-		$.scrollTo(top - 60 > 0 ? top - 60 : 0, 800);
+		$.scrollTo(top - 58 > 0 ? top - 58 : 0, 800);
 
 		window.location.href = window.location.origin + window.location.pathname + new_selected_id;
 		return false;

a2f60699f7ab8da967db3110dc3957d17bd7aa5b
Linda Xie <lindaxie93@gmail.com>
2013-08-29 15:37:28 -0400

    updated resume; removed hash when scrolling

--- public/js/linda-script.js
+++ public/js/linda-script.js
@@ -7,28 +7,28 @@ $(function(){
 
 		// Remove old content (by fading out)
 		var active_content = $('section.active');
-		//console.log('Hiding: ');
 		//console.log(active_content);
-		//active_content.removeClass('active').hide();
 
 		// Remove selected from old menus, and add them to new one
 		var new_selected_id = $this.attr('href'); // "#about"
-		console.log(new_selected_id);
 		var old_selected_menus = $('.nav-items a.selected');
-		console.log(old_selected_menus);
 		old_selected_menus.removeClass('selected');
 		$(new_selected_id).addClass('selected');
 
 		// Scroll to the correct position
+		//parent.location.hash = id_without_hash; //changes browser hash
+		console.log(parent.location.hash); 
+
 		var top = $(new_selected_id).offset().top;
 		$.scrollTo(top - 58 > 0 ? top - 58 : 0, 800);
 
-		window.location.href = window.location.origin + window.location.pathname + new_selected_id;
+
+		//window.location.href = window.location.origin + window.location.pathname + new_selected_id;
 		return false;
 	});
 
 	// Load page based on anchor/hash tag
-	 if(window.location.hash) {
+	 if(window.location.pathname) {
 	 	$('.nav-items a[href="' + window.location.hash + '"]').click();
 	}
 
